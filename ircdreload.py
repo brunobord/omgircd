626a020316b7960617cb9f08bd1199735af1e0fc
Bruno Bord <bruno@jehaisleprintemps.net>
2015-10-22 21:38:56 +0200

    pep8

--- ircdreload.py
+++ ircdreload.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 #
 #       ircdreload.py
-#       
+#
 #       Copyright 2010 Curtis (Programble) <programble@gmail.com>
-#       
+#
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 2 of the License, or
 #       (at your option) any later version.
-#       
+#
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #       GNU General Public License for more details.
-#       
+#
 #       You should have received a copy of the GNU General Public License
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -55,7 +55,6 @@ while True:
                     channel.usermodes[newuser] = channel.usermodes[olduser]
                     channel.usermodes.pop(olduser)
                     newuser.channels.append(channel)
-                #server.users.append(newuser)
             old.close()
             continue
 server.shutdown()
